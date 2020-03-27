1a5c7d2758d0b422b177832cb16cff921d62c9b3
DeividsonGonini <deividsongonini@gmail.com>
2020-03-27 15:27:37 -0300

    Mobile da Aplicação

---
+++ mobile/App.js
@@ -0,0 +1,15 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Routs from './src/routes';
+
+//Estrutura
+export default function App() {
+  return (
+    <Routs />
+  );
+}
+
